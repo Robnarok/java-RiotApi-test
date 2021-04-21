3d7d0f208b8a8e72637b69f1771b26a6f62f505e
Robin Kraft <rokra102@hhu.de>
2021-04-21 16:52:36 +0200

    initial commit & gitignore update

---
+++ yoneban/src/main/java/com/robnarok/yoneban/YonebanApplication.java
@@ -0,0 +1,13 @@
+package com.robnarok.yoneban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonebanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonebanApplication.class, args);
+	}
+
+}
---
+++ yoneban/src/test/java/com/robnarok/yoneban/YonebanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robnarok.yoneban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonebanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
